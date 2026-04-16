7329b9e29804cd7683a5c1e445b1e8bae8395509
koskedk <koskedk@live.com>
2026-04-16 18:27:09 +0300

    demo start

---
+++ CarvedRock.McpServer/Program.cs
@@ -0,0 +1,33 @@
+using CarvedRock.McpServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddMcpServer()
+    .WithHttpTransport(options =>options.Stateless=true )
+    .WithTools<CarvedRockTools>();
+
+builder.Services.AddHttpClient("CarvedRockApi", client =>
+    client.BaseAddress = new Uri("https://api")
+);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("McpInspector", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:6274")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+
+var app = builder.Build();
+app.UseCors("McpInspector");
+app.MapDefaultEndpoints();
+app.MapMcp();
+
+app.Run();
